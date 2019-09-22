da2376b1000a2826b3a5003887c7e267ec3dc03b
Kennedy Njeri <mistakenz123@gmail.com>
2019-09-22 15:07:49 +0300

    get request dispatch

--- src/reducers/contactReducer.js
+++ src/reducers/contactReducer.js
@@ -3,24 +3,6 @@ import {GET_CONTACTS, ADD_CONTACTS, DELETE_CONTACT} from "../actions/type";
 
 const initialState = {
     contacts: [
-        {
-            id: 1,
-            name: 'John Doe1',
-            email: 'john@gmail.com',
-            phone: '555-555-5555'
-        },
-        {
-            id: 2,
-            name: 'Karen Williams',
-            email: 'karen@gmail.com',
-            phone: '444-444-4444'
-        },
-        {
-            id: 3,
-            name: 'Henry Johnson',
-            email: 'henry@gmail.com',
-            phone: '333-333-333'
-        }
     ]
 }
 
@@ -29,7 +11,8 @@ export default function (state = initialState, action) {
     switch (action.type) {
         case GET_CONTACTS:
             return {
-                ...state
+                ...state,
+                contacts: action.payload
             }
         case DELETE_CONTACT:
             return {
